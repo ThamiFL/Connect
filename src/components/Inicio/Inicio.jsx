e4d0bc8508a5b9c2687545c04a867aade5853c37
Thamires Brito <thamifl29@gmail.com>
2026-02-04 20:38:45 -0300

    Terminando o perfil

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -1,15 +1,22 @@
 import styles from "./Inicio.module.scss";
-import ImgInicial from "../../assets/img-tela-inicial.png"
+import ImgInicial from "../../assets/img-tela-inicial.png";
 
 export default function Inicio() {
   return (
     <section className={styles.sectionDoInicio}>
-      <h1> Projetos Sociais que transformam </h1>
-      <p>
-        Conectamos sua empresa com projetos sociais impactantes. Juntos, criamos
-        mudanças reais na comunidade.
-      </p>
-      <img className={styles.ImgInicial} src={ImgInicial} alt="" />
+
+      <div className={styles.divDoIninio}>
+
+        <h1 className={styles.titulo}> Projetos Sociais <p> que transformam </p></h1>
+        <p className={styles.paragrafo}>
+          Conectamos sua empresa com projetos sociais impactantes. Juntos,
+          criamos mudanças reais na comunidade.
+        </p>
+
+      </div>
+
+      <img className={styles.ImgInicial} src={ImgInicial} alt="Foto de duas mãos segurando um globo" />
+
     </section>
   );
 }
--- src/components/Perfil/Perfil.jsx
+++ src/components/Perfil/Perfil.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import styles from "./Perfil.module.scss";
+import perfil from "../../assets/fotodeperfil.png";
 
 export default function Perfil() {
+  return (
+    <section className={styles.secPerfil}>
+      <h1> Thamires Brito </h1>
+      <h2> </h2>
+      <p>
+           Apaixonado por fazer a diferença na comunidade. Acredito que pequenas
+        ações podem transformar vidas e estou sempre em busca de novas
+        oportunidades para ajudar.
+      </p>
+      <address> </address>
 
-    return (
-        <>
-        
-        <h2> Perfil </h2>
-        </>
-    )
+      <ul className={styles.linksPerfil}>
+        <li>Educação</li>
+        <li>Meio Ambiente</li>
+        <li>Assistência Social</li>
+        <li>Design</li>
+      </ul>
 
+      <img className={styles.imagemPerfil} src={perfil} alt="" />
+    </section>
+  );
 }
