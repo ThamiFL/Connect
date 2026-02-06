22ca5e7c72e09700acb4ed2f90a8e027be7c178d
Thamires Brito <thamifl29@gmail.com>
2026-02-06 19:41:18 -0300

    Perfil e inicio

--- src/components/AcoesDaConnect/AcoesDaConnect.jsx
+++ src/components/AcoesDaConnect/AcoesDaConnect.jsx
@@ -1,11 +1,29 @@
-import React from "react";
-
 export default function AcoesDeConnect() {
 
     return (
-        <>
-        <h2> Ações </h2>
-        </>
+        <section>
+        <h1> Ações da Connect </h1>
+<p> Junte-se a quem acredita na mudança. Apoie, participe e transforme vidas através de ações sociais que geram impacto real nas comunidades. </p>
+
+
+
+
+<img src="" alt="" />
+<h2></h2>
+<p></p>
+<button></button>
+
+<img src="" alt="" />
+<img src="" alt="" />
+<img src="" alt="" />
+<img src="" alt="" />
+<img src="" alt="" />
+
+
+
+
+
+        </section>
     )
 
 }
--- src/components/Perfil/Perfil.jsx
+++ src/components/Perfil/Perfil.jsx
@@ -4,23 +4,31 @@ import perfil from "../../assets/fotodeperfil.png";
 export default function Perfil() {
   return (
     <section className={styles.secPerfil}>
-      <h1> Thamires Brito </h1>
-      <h2> </h2>
-      <p>
-           Apaixonado por fazer a diferença na comunidade. Acredito que pequenas
-        ações podem transformar vidas e estou sempre em busca de novas
-        oportunidades para ajudar.
-      </p>
-      <address> </address>
+      <article className={styles.articleperfil}>
+        <img className={styles.imagemPerfil} src={perfil} alt="" />
+        <div className={styles.divperfil}>
+          <h1 className={styles.h1perfil}> Thamires Brito </h1>
+          <h3 className={styles.h3}> Voluntária Ativo</h3>
+          <p className={styles.p1}>
+            Apaixonada por fazer a diferença na comunidade. Acredito que
+            pequenas ações podem transformar vidas e estou sempre em busca de
+            novas oportunidades para ajudar.
+          </p>
 
-      <ul className={styles.linksPerfil}>
-        <li>Educação</li>
-        <li>Meio Ambiente</li>
-        <li>Assistência Social</li>
-        <li>Design</li>
-      </ul>
+          <address>
+            <p>São Gonçalo, RJ</p>
+            <p>thamifl29@gmail.com </p>
+            <p>Membro desde Janeiro 2022 </p>
+          </address>
 
-      <img className={styles.imagemPerfil} src={perfil} alt="" />
+          <ul className={styles.linksPerfil}>
+            <li>Educação</li>
+            <li>Meio Ambiente</li>
+            <li>Assistência Social</li>
+            <li>Design</li>
+          </ul>
+        </div>
+      </article>
     </section>
   );
 }
