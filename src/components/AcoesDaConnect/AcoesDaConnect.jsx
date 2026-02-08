4c259ca4ee37b5f3a1fa6832fda72a1bd1402f01
Thamires Brito <thamifl29@gmail.com>
2026-02-08 02:03:43 -0300

    Acrescentando infomaçoes

--- src/components/AcoesDaConnect/AcoesDaConnect.jsx
+++ src/components/AcoesDaConnect/AcoesDaConnect.jsx
@@ -1,29 +1,49 @@
+import styles from "./AcoesDaConnect.module.scss"
+
+
 export default function AcoesDeConnect() {
+  return (
+    <section className={styles.secAcoes}>
+        
+      <h1 className={styles.h1Acoes}> Ações da Connect </h1>
+      <p className={styles.pAcoes}>
 
-    return (
-        <section>
-        <h1> Ações da Connect </h1>
-<p> Junte-se a quem acredita na mudança. Apoie, participe e transforme vidas através de ações sociais que geram impacto real nas comunidades. </p>
+        Junte-se a quem acredita na mudança. Apoie, participe e transforme vidas
+        através de ações sociais que geram impacto real nas comunidades.{" "}
+      </p>
 
+<article className={styles.articleAcoes}>
 
+   <img src="" alt="" />
+      <h2>Mutirão de reciclagem</h2>
+      <p>Coletar materiais recicláveis e orientar sobre descarte consciente.</p>
+      <button className={styles.botao}>Quero participar</button>
 
+      <img src="" alt="" />
+      <h2>Aulas de Tecnologia</h2>
+      <p>Ensinar noções básicas de informática, internet segura e programação. Preparar jovens para o mercado de trabalho digital.</p>
+      <button className={styles.botao}>Quero participar</button>
 
-<img src="" alt="" />
-<h2></h2>
-<p></p>
-<button></button>
+      <img src="" alt="" />
+      <h2>Esporte e Inclusão</h2>
+      <p>Organizar treinos e jogos que incentivem a participação de todos. Promover disciplina, saúde e trabalho em equipe entre jovens.</p>
+      <button className={styles.botao}>Quero participar</button>
 
-<img src="" alt="" />
-<img src="" alt="" />
-<img src="" alt="" />
-<img src="" alt="" />
-<img src="" alt="" />
+      <img src="" alt="" />
+      <h2>Instituto grande familia</h2>
+      <p>Contribua com alimentos não perecíveis e ajude famílias em situação de vulnerabilidade.</p>
+      <button className={styles.botao}>Quero participar</button>
 
+      <img src="" alt="" />
+      <h2>Projeto Futuro na Escola</h2>
+      <p>Doe livros, cadernos, lápis, mochilas e canetas para ajudar jovens a continuarem seus estudos com mais estrutura.</p>
+      <button className={styles.botao}>Quero participar</button>
 
-
-
-
-        </section>
-    )
-
+      <img src="" alt="" />
+      <h2>Instituto Conecta Jovem</h2>
+      <p>Doe livros, cadernos, lápis, mochilas e canetas para ajudar jovens a continuarem seus estudos com mais estrutura.</p>
+      <button className={styles.botao}>Quero participar</button>
+   </article> 
+   </section>
+  );
 }
